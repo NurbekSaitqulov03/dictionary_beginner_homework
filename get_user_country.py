f4d265357a705c36493a9ee84a9dcd7b999f7961
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-01-06 22:42:08 +0500

    get_user_country.py

--- get_user_country.py
+++ get_user_country.py
@@ -8,4 +8,35 @@ def get_user_country(data:list, name:str) -> list:
     Returns:
         str: The country of the user with the given name
     """
-    return 
+    x = 0
+    for i in data:
+        x = list(i.values())
+        if name == x[0]:
+            return x[-1]
+ab = [
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Henry', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Sam', 
+    'country': 'MEX'
+  },
+  {
+    'name': 'Kevin', 
+    'country': 'RUS'
+  },
+  {
+    'name': 'Dustin', 
+    'country': 'GER'
+  }
+]
+print(get_user_country(ab, "Henry"))
