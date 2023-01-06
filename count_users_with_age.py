6a68824ace43c3d9c3bbdf92a45c8954fbcca20b
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-01-06 21:07:33 +0500

    count_users_with_age.py

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -8,4 +8,20 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    return 
+    x = 0
+    for i in data:
+        for w in i.values():
+            if w == age:
+                x+=1
+    return x
+aba = [
+  {
+    'name': 'John', 
+    'age': 35
+  },
+  {
+    'name':'Mary', 
+    'age': 20
+  }
+  ]
+print(count_users_with_age(aba, 38))
