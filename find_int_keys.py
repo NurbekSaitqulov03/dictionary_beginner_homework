f9036c9ccb102dd9f1c7396e2b2030f96368b3ba
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-01-06 23:27:18 +0500

    find_int_keys.py

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,16 +6,16 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    x = list(data.keys())
-    a = []
-    for i in x:
-        if str(i).isdigit():
-            a.append(i)
-    return a
-san = {
+    a = list(data.keys())
+    b = []
+    for i in a:
+        if type(i)==int:
+            b.append(i)
+    return b
+anm = {
     'a': 1, 
     3 : 2, 
     'c': 3,
     10:'a'
   }
-print(find_int_keys(san))
+print(find_int_keys(anm))
